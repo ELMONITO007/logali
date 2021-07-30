e54c8029b3ede3cb5080f3b9d91d0877e00bfbf7
elmonito007 <elmonito007@hotmail.com>
2021-07-30 15:16:47 +0000

    labo elementos

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -1,15 +1,31 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
+        function myCheck() {
 
-		return Controller.extend("logaligroup.employees.controller.MainView", {
-			onInit: function () {
+            var inputEmployeed = this.byId("imputEmployeed");
+            var valuesEmployeed = inputEmployeed.getValue();
+            if (valuesEmployeed.length >= 6) {
+                //inputEmployeed.setDescription("OK");
+                this.byId("labelCountry").setVisible(true);
+                this.byId("slCountry").setVisible(true);
 
-			}
-		});
-	});
+            } else {
+                //inputEmployeed.setDescription("Error");
+                 this.byId("labelCountry").setVisible(false);
+                this.byId("slCountry").setVisible(false);
+            }
+
+        }
+        return Controller.extend("logaligroup.employees.controller.MainView", {
+            onInit: function () {
+
+            },
+            onValidate: myCheck
+        });
+    });
